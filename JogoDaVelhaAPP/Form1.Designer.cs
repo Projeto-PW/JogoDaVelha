5dfd5da13f76978afcc9cd01633855e2a82883bf
1DS <1ds@bglab.local>
2025-09-11 10:42:47 -0300

    Criando Jogo da Velha

---
+++ JogoDaVelhaAPP/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace JogoDaVelhaAPP
+{
+    partial class frmJogo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnJogo1 = new System.Windows.Forms.Button();
+            this.btnJogo2 = new System.Windows.Forms.Button();
+            this.btnJogo3 = new System.Windows.Forms.Button();
+            this.btnJogo6 = new System.Windows.Forms.Button();
+            this.btnJogo5 = new System.Windows.Forms.Button();
+            this.btnJogo4 = new System.Windows.Forms.Button();
+            this.btnJogo9 = new System.Windows.Forms.Button();
+            this.btnJogo8 = new System.Windows.Forms.Button();
+            this.btnJogo7 = new System.Windows.Forms.Button();
+            this.btnComecar = new System.Windows.Forms.Button();
+            this.btnRecomecar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblJogador = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnJogo1
+            // 
+            this.btnJogo1.BackColor = System.Drawing.Color.White;
+            this.btnJogo1.Enabled = false;
+            this.btnJogo1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo1.Location = new System.Drawing.Point(122, 86);
+            this.btnJogo1.Name = "btnJogo1";
+            this.btnJogo1.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo1.TabIndex = 0;
+            this.btnJogo1.UseVisualStyleBackColor = false;
+            this.btnJogo1.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo2
+            // 
+            this.btnJogo2.BackColor = System.Drawing.Color.White;
+            this.btnJogo2.Enabled = false;
+            this.btnJogo2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo2.Location = new System.Drawing.Point(203, 86);
+            this.btnJogo2.Name = "btnJogo2";
+            this.btnJogo2.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo2.TabIndex = 1;
+            this.btnJogo2.UseVisualStyleBackColor = false;
+            this.btnJogo2.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo3
+            // 
+            this.btnJogo3.BackColor = System.Drawing.Color.White;
+            this.btnJogo3.Enabled = false;
+            this.btnJogo3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo3.Location = new System.Drawing.Point(284, 86);
+            this.btnJogo3.Name = "btnJogo3";
+            this.btnJogo3.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo3.TabIndex = 2;
+            this.btnJogo3.UseVisualStyleBackColor = false;
+            this.btnJogo3.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo6
+            // 
+            this.btnJogo6.BackColor = System.Drawing.Color.White;
+            this.btnJogo6.Enabled = false;
+            this.btnJogo6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo6.Location = new System.Drawing.Point(284, 167);
+            this.btnJogo6.Name = "btnJogo6";
+            this.btnJogo6.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo6.TabIndex = 5;
+            this.btnJogo6.UseVisualStyleBackColor = false;
+            this.btnJogo6.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo5
+            // 
+            this.btnJogo5.BackColor = System.Drawing.Color.White;
+            this.btnJogo5.Enabled = false;
+            this.btnJogo5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo5.Location = new System.Drawing.Point(203, 167);
+            this.btnJogo5.Name = "btnJogo5";
+            this.btnJogo5.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo5.TabIndex = 4;
+            this.btnJogo5.UseVisualStyleBackColor = false;
+            this.btnJogo5.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo4
+            // 
+            this.btnJogo4.BackColor = System.Drawing.Color.White;
+            this.btnJogo4.Enabled = false;
+            this.btnJogo4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo4.Location = new System.Drawing.Point(122, 167);
+            this.btnJogo4.Name = "btnJogo4";
+            this.btnJogo4.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo4.TabIndex = 3;
+            this.btnJogo4.UseVisualStyleBackColor = false;
+            this.btnJogo4.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo9
+            // 
+            this.btnJogo9.BackColor = System.Drawing.Color.White;
+            this.btnJogo9.Enabled = false;
+            this.btnJogo9.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo9.Location = new System.Drawing.Point(284, 248);
+            this.btnJogo9.Name = "btnJogo9";
+            this.btnJogo9.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo9.TabIndex = 8;
+            this.btnJogo9.UseVisualStyleBackColor = false;
+            this.btnJogo9.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo8
+            // 
+            this.btnJogo8.BackColor = System.Drawing.Color.White;
+            this.btnJogo8.Enabled = false;
+            this.btnJogo8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo8.Location = new System.Drawing.Point(203, 248);
+            this.btnJogo8.Name = "btnJogo8";
+            this.btnJogo8.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo8.TabIndex = 7;
+            this.btnJogo8.UseVisualStyleBackColor = false;
+            this.btnJogo8.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnJogo7
+            // 
+            this.btnJogo7.BackColor = System.Drawing.Color.White;
+            this.btnJogo7.Enabled = false;
+            this.btnJogo7.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnJogo7.Location = new System.Drawing.Point(122, 248);
+            this.btnJogo7.Name = "btnJogo7";
+            this.btnJogo7.Size = new System.Drawing.Size(75, 75);
+            this.btnJogo7.TabIndex = 6;
+            this.btnJogo7.UseVisualStyleBackColor = false;
+            this.btnJogo7.Click += new System.EventHandler(this.btnJogo_Click);
+            // 
+            // btnComecar
+            // 
+            this.btnComecar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnComecar.Location = new System.Drawing.Point(467, 106);
+            this.btnComecar.Name = "btnComecar";
+            this.btnComecar.Size = new System.Drawing.Size(184, 49);
+            this.btnComecar.TabIndex = 9;
+            this.btnComecar.Text = "Começar Jogo";
+            this.btnComecar.UseVisualStyleBackColor = true;
+            this.btnComecar.Click += new System.EventHandler(this.btnComecar_Click);
+            // 
+            // btnRecomecar
+            // 
+            this.btnRecomecar.Enabled = false;
+            this.btnRecomecar.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRecomecar.Location = new System.Drawing.Point(467, 187);
+            this.btnRecomecar.Name = "btnRecomecar";
+            this.btnRecomecar.Size = new System.Drawing.Size(184, 49);
+            this.btnRecomecar.TabIndex = 10;
+            this.btnRecomecar.Text = "Recomeçar Jogo";
+            this.btnRecomecar.UseVisualStyleBackColor = true;
+            this.btnRecomecar.Click += new System.EventHandler(this.btnRecomecar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(467, 268);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(184, 49);
+            this.btnSair.TabIndex = 11;
+            this.btnSair.Text = "Sair do Jogo";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // lblJogador
+            // 
+            this.lblJogador.AutoSize = true;
+            this.lblJogador.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJogador.Location = new System.Drawing.Point(124, 379);
+            this.lblJogador.Name = "lblJogador";
+            this.lblJogador.Size = new System.Drawing.Size(275, 24);
+            this.lblJogador.TabIndex = 12;
+            this.lblJogador.Text = "Clique para começar o jogo!";
+            // 
+            // frmJogo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblJogador);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnRecomecar);
+            this.Controls.Add(this.btnComecar);
+            this.Controls.Add(this.btnJogo9);
+            this.Controls.Add(this.btnJogo8);
+            this.Controls.Add(this.btnJogo7);
+            this.Controls.Add(this.btnJogo6);
+            this.Controls.Add(this.btnJogo5);
+            this.Controls.Add(this.btnJogo4);
+            this.Controls.Add(this.btnJogo3);
+            this.Controls.Add(this.btnJogo2);
+            this.Controls.Add(this.btnJogo1);
+            this.Name = "frmJogo";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da Velha";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnJogo1;
+        private System.Windows.Forms.Button btnJogo2;
+        private System.Windows.Forms.Button btnJogo3;
+        private System.Windows.Forms.Button btnJogo6;
+        private System.Windows.Forms.Button btnJogo5;
+        private System.Windows.Forms.Button btnJogo4;
+        private System.Windows.Forms.Button btnJogo9;
+        private System.Windows.Forms.Button btnJogo8;
+        private System.Windows.Forms.Button btnJogo7;
+        private System.Windows.Forms.Button btnComecar;
+        private System.Windows.Forms.Button btnRecomecar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblJogador;
+    }
+}
+
---
+++ JogoDaVelhaAPP/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace JogoDaVelhaAPP
+{
+    public partial class frmJogo : Form
+    {
+        int jogador = 1;
+        public frmJogo()
+        {
+            InitializeComponent();
+        }
+
+        private void btnJogo_Click(object sender, EventArgs e)
+        {
+            Button clickedButton = sender as Button;
+            switch(jogador)
+            {
+                case 1:
+                    clickedButton.Text = "X";
+                    clickedButton.ForeColor = Color.Red;
+                    lblJogador.ForeColor = Color.Blue;
+                    jogador++;
+                break;
+                case 2:
+                    clickedButton.Text = "O";
+                    clickedButton.ForeColor = Color.Blue;
+                    lblJogador.ForeColor = Color.Red;
+                    jogador--;
+                    break;
+            }
+            clickedButton.Enabled = false;
+            lblJogador.Text = "Jogador " + jogador.ToString() + " joga";
+        }
+
+        private void btnComecar_Click(object sender, EventArgs e)
+        {
+            btnJogo1.Enabled = true;
+            btnJogo2.Enabled = true;
+            btnJogo3.Enabled = true;
+            btnJogo4.Enabled = true;
+            btnJogo5.Enabled = true;
+            btnJogo6.Enabled = true;
+            btnJogo7.Enabled = true;
+            btnJogo8.Enabled = true;
+            btnJogo9.Enabled = true;
+            btnComecar.Enabled = false;
+            btnRecomecar.Enabled = true;
+            lblJogador.Text = "Jogador " + jogador.ToString() + " Joga";
+            lblJogador.ForeColor = Color.Red;
+        }
+
+        private void btnRecomecar_Click(object sender, EventArgs e)
+        {
+            btnJogo1.Enabled = false;
+            btnJogo1.Text = "";
+            btnJogo2.Enabled = false;
+            btnJogo2.Text = "";
+            btnJogo3.Enabled = false;
+            btnJogo3.Text = "";
+            btnJogo4.Enabled = false;
+            btnJogo4.Text = "";
+            btnJogo5.Enabled = false;
+            btnJogo5.Text = "";
+            btnJogo6.Enabled = false;
+            btnJogo6.Text = "";
+            btnJogo7.Enabled = false;
+            btnJogo7.Text = "";
+            btnJogo8.Enabled = false;
+            btnJogo8.Text = "";
+            btnJogo9.Enabled = false;
+            btnJogo9.Text = "";
+            btnComecar.Enabled = true;
+            btnRecomecar.Enabled = false;
+            lblJogador.Text = "Clique para começar o jogo";
+            lblJogador.ForeColor = Color.Black;
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
